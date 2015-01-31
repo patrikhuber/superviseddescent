1af23f000e341c9dc4ea6b8e05679191aeecf282
Patrik Huber <patrikhuber@gmail.com>
2015-01-31 23:38:20 +0000

    Updated documentation to today's build

--- doc/search/functions_3.js
+++ doc/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['predict',['predict',['../classsuperviseddescent_1_1_regressor.html#ac6e342e2d18ed634df6300973e1777cc',1,'superviseddescent::Regressor::predict()'],['../classsuperviseddescent_1_1_linear_regressor.html#ac623ad269cbf1e93e34275335c3a8c50',1,'superviseddescent::LinearRegressor::predict()'],['../classsuperviseddescent_1_1_supervised_descent_optimiser.html#a6e783e26433fe7b9be0b831ae30dd895',1,'superviseddescent::SupervisedDescentOptimiser::predict()']]]
+  ['predict',['predict',['../classsuperviseddescent_1_1_regressor.html#ac6e342e2d18ed634df6300973e1777cc',1,'superviseddescent::Regressor::predict()'],['../classsuperviseddescent_1_1_linear_regressor.html#a4332e48c2d3e733927bc0905451456d7',1,'superviseddescent::LinearRegressor::predict()'],['../classsuperviseddescent_1_1_supervised_descent_optimiser.html#a6e783e26433fe7b9be0b831ae30dd895',1,'superviseddescent::SupervisedDescentOptimiser::predict()']]]
 ];
