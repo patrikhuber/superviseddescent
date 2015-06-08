3098994f01663aecaa6fabf22669fc483669b172
Patrik Huber <patrikhuber@gmail.com>
2015-06-08 20:44:13 +0100

    Renamed most variables in the example apps to snake_case

--- examples/landmark_detection.cpp
+++ examples/landmark_detection.cpp
@@ -62,7 +62,7 @@ using std::endl;
  * @param[in] filename Path to a .pts file.
  * @return An ordered vector with the 68 ibug landmarks.
  */
-vector<Vec2f> readPtsLandmarks(std::string filename)
+vector<Vec2f> read_pts_landmarks(std::string filename)
 {
 	using std::getline;
 	vector<Vec2f> landmarks;
@@ -84,9 +84,9 @@ vector<Vec2f> readPtsLandmarks(std::string filename)
 		if (line == "}") { // end of the file
 			break;
 		}
-		std::stringstream lineStream(line);
+		std::stringstream line_stream(line);
 		Vec2f landmark(0.0f, 0.0f);
-		if (!(lineStream >> landmark[0] >> landmark[1])) {
+		if (!(line_stream >> landmark[0] >> landmark[1])) {
 			throw std::runtime_error(string("Landmark format error while parsing the line: " + line));
 		}
 		// From the iBug website:
@@ -108,34 +108,34 @@ vector<Vec2f> readPtsLandmarks(std::string filename)
  * @param[in] directory A directory with .png images and ibug .pts files.
  * @return A pair with the loaded images and their landmarks (all in one cv::Mat).
  */
-std::pair<vector<Mat>, Mat> loadIbugData(fs::path directory)
+std::pair<vector<Mat>, Mat> load_ibug_data(fs::path directory)
 {
 	vector<Mat> images;
 	Mat landmarks;
 
 	// Get all the filenames in the given directory:
-	vector<fs::path> imageFilenames;
+	vector<fs::path> image_filenames;
 	fs::directory_iterator end_itr;
 	for (fs::directory_iterator i(directory); i != end_itr; ++i)
 	{
 		if (fs::is_regular_file(i->status()) && i->path().extension() == ".png")
-			imageFilenames.emplace_back(i->path());
+			image_filenames.emplace_back(i->path());
 	}
 
 	// Load each image and its landmarks into memory:
-	for (auto file : imageFilenames)
+	for (auto file : image_filenames)
 	{
 		images.emplace_back(cv::imread(file.string()));
 		// We load the landmarks and convert them into [x_0, ..., x_n, y_0, ..., y_n] format:
 		file.replace_extension(".pts");
-		auto lms = readPtsLandmarks(file.string());
-		int numLandmarks = static_cast<int>(lms.size());
-		Mat landmarksAsRow(1, 2 * numLandmarks, CV_32FC1);
-		for (int i = 0; i < numLandmarks; ++i) {
-			landmarksAsRow.at<float>(i) = lms[i][0]; // the x coordinate
-			landmarksAsRow.at<float>(i + numLandmarks) = lms[i][1]; // y coordinate
+		auto lms = read_pts_landmarks(file.string());
+		int num_landmarks = static_cast<int>(lms.size());
+		Mat landmarks_as_row(1, 2 * num_landmarks, CV_32FC1);
+		for (int i = 0; i < num_landmarks; ++i) {
+			landmarks_as_row.at<float>(i) = lms[i][0]; // the x coordinate
+			landmarks_as_row.at<float>(i + num_landmarks) = lms[i][1]; // y coordinate
 		}
-		landmarks.push_back(landmarksAsRow);
+		landmarks.push_back(landmarks_as_row);
 	}
 	return std::make_pair(images, landmarks);
 };
@@ -163,12 +163,12 @@ public:
 	 * Note: Only VlHogVariantUoctti is tested.
 	 *
 	 * @param[in] images A vector of images used for training or testing.
-	 * @param[in] vlHogVariant The VLFeat HoG variant.
-	 * @param[in] numCells Number of HoG cells that should be constructed around each landmark.
-	 * @param[in] cellSize Width of one HoG cell in pixels.
-	 * @param[in] numBins Number of orientations of a HoG cell.
+	 * @param[in] vlhog_variant The VLFeat HoG variant.
+	 * @param[in] num_cells Number of HoG cells that should be constructed around each landmark.
+	 * @param[in] cell_size Width of one HoG cell in pixels.
+	 * @param[in] num_bins Number of orientations of a HoG cell.
 	 */
-	HogTransform(vector<Mat> images, VlHogVariant vlHogVariant, int numCells, int cellSize, int numBins) : images(images), vlHogVariant(vlHogVariant), numCells(numCells), cellSize(cellSize), numBins(numBins)
+	HogTransform(vector<Mat> images, VlHogVariant vlhog_variant, int num_cells, int cell_size, int num_bins) : images(images), vlhog_variant(vlhog_variant), num_cells(num_cells), cell_size(cell_size), num_bins(num_bins)
 	{
 	};
 
@@ -181,55 +181,55 @@ public:
 	 * [x_0, ..., x_n, y_0, ..., y_n].
 	 *
 	 * @param[in] parameters The current 2D landmark position estimate.
-	 * @param[in] regressorLevel Not used in this example.
-	 * @param[in] trainingIndex Gets supplied by the SupervisedDescent optimiser during training and testing, to know from which image to extract features.
+	 * @param[in] regressor_level Not used in this example.
+	 * @param[in] training_index Gets supplied by the SupervisedDescent optimiser during training and testing, to know from which image to extract features.
 	 * @return Returns the HoG features at the given 2D locations.
 	 */
-	cv::Mat operator()(cv::Mat parameters, size_t regressorLevel, int trainingIndex = 0)
+	cv::Mat operator()(cv::Mat parameters, size_t regressor_level, int training_index = 0)
 	{
 		assert(parameters.rows == 1);
 		using cv::Mat;
 			
-		Mat grayImage;
-		if (images[trainingIndex].channels() == 3) {
-			cv::cvtColor(images[trainingIndex], grayImage, cv::COLOR_BGR2GRAY);
+		Mat gray_image;
+		if (images[training_index].channels() == 3) {
+			cv::cvtColor(images[training_index], gray_image, cv::COLOR_BGR2GRAY);
 		}
 		else {
-			grayImage = images[trainingIndex];
+			gray_image = images[training_index];
 		}
 
 		// Note: We could use the 'regressorLevel' to choose the window size (and
 		// other parameters adaptively). We omit this for the sake of a short example.
 
-		int patchWidthHalf = numCells * (cellSize / 2);
+		int patch_width_half = num_cells * (cell_size / 2);
 
-		Mat hogDescriptors; // We'll get the dimensions later from vl_hog_get_*
+		Mat hog_descriptors; // We'll get the dimensions later from vl_hog_get_*
 
-		int numLandmarks = parameters.cols / 2;
-		for (int i = 0; i < numLandmarks; ++i) {
+		int num_landmarks = parameters.cols / 2;
+		for (int i = 0; i < num_landmarks; ++i) {
 			int x = cvRound(parameters.at<float>(i));
-			int y = cvRound(parameters.at<float>(i + numLandmarks));
+			int y = cvRound(parameters.at<float>(i + num_landmarks));
 
-			Mat roiImg;
-			if (x - patchWidthHalf < 0 || y - patchWidthHalf < 0 || x + patchWidthHalf >= grayImage.cols || y + patchWidthHalf >= grayImage.rows) {
+			Mat roi_img;
+			if (x - patch_width_half < 0 || y - patch_width_half < 0 || x + patch_width_half >= gray_image.cols || y + patch_width_half >= gray_image.rows) {
 				// The feature extraction location is too far near a border. We extend the
 				// image (add a black canvas) and then extract from this larger image.
-				int borderLeft = (x - patchWidthHalf) < 0 ? std::abs(x - patchWidthHalf) : 0; // x and y are patch-centers
-				int borderTop = (y - patchWidthHalf) < 0 ? std::abs(y - patchWidthHalf) : 0;
-				int borderRight = (x + patchWidthHalf) >= grayImage.cols ? std::abs(grayImage.cols - (x + patchWidthHalf)) : 0;
-				int borderBottom = (y + patchWidthHalf) >= grayImage.rows ? std::abs(grayImage.rows - (y + patchWidthHalf)) : 0;
-				Mat extendedImage = grayImage.clone();
+				int borderLeft = (x - patch_width_half) < 0 ? std::abs(x - patch_width_half) : 0; // x and y are patch-centers
+				int borderTop = (y - patch_width_half) < 0 ? std::abs(y - patch_width_half) : 0;
+				int borderRight = (x + patch_width_half) >= gray_image.cols ? std::abs(gray_image.cols - (x + patch_width_half)) : 0;
+				int borderBottom = (y + patch_width_half) >= gray_image.rows ? std::abs(gray_image.rows - (y + patch_width_half)) : 0;
+				Mat extendedImage = gray_image.clone();
 				cv::copyMakeBorder(extendedImage, extendedImage, borderTop, borderBottom, borderLeft, borderRight, cv::BORDER_CONSTANT, cv::Scalar(0));
-				cv::Rect roi((x - patchWidthHalf) + borderLeft, (y - patchWidthHalf) + borderTop, patchWidthHalf * 2, patchWidthHalf * 2); // Rect: x y w h. x and y are top-left corner.
-				roiImg = extendedImage(roi).clone(); // clone because we need a continuous memory block
+				cv::Rect roi((x - patch_width_half) + borderLeft, (y - patch_width_half) + borderTop, patch_width_half * 2, patch_width_half * 2); // Rect: x y w h. x and y are top-left corner.
+				roi_img = extendedImage(roi).clone(); // clone because we need a continuous memory block
 			}
 			else {
-				cv::Rect roi(x - patchWidthHalf, y - patchWidthHalf, patchWidthHalf * 2, patchWidthHalf * 2); // x y w h. Rect: x and y are top-left corner. Our x and y are center. Convert.
-				roiImg = grayImage(roi).clone(); // clone because we need a continuous memory block
+				cv::Rect roi(x - patch_width_half, y - patch_width_half, patch_width_half * 2, patch_width_half * 2); // x y w h. Rect: x and y are top-left corner. Our x and y are center. Convert.
+				roi_img = gray_image(roi).clone(); // clone because we need a continuous memory block
 			}
-			roiImg.convertTo(roiImg, CV_32FC1); // vl_hog_put_image expects a float* (values 0.0f-255.0f)
-			VlHog* hog = vl_hog_new(vlHogVariant, numBins, false); // transposed (=col-major) = false
-			vl_hog_put_image(hog, (float*)roiImg.data, roiImg.cols, roiImg.rows, 1, cellSize); // (the '1' is numChannels)
+			roi_img.convertTo(roi_img, CV_32FC1); // vl_hog_put_image expects a float* (values 0.0f-255.0f)
+			VlHog* hog = vl_hog_new(vlhog_variant, num_bins, false); // transposed (=col-major) = false
+			vl_hog_put_image(hog, (float*)roi_img.data, roi_img.cols, roi_img.rows, 1, cell_size); // (the '1' is numChannels)
 			int ww = static_cast<int>(vl_hog_get_width(hog)); // assert ww == hh == numCells
 			int hh = static_cast<int>(vl_hog_get_height(hog));
 			int dd = static_cast<int>(vl_hog_get_dimension(hog)); // assert ww=hogDim1, hh=hogDim2, dd=hogDim3
@@ -246,19 +246,19 @@ public:
 				hogFeatures.copyTo(currentDimSubMat);
 			}
 			hogDescriptor = hogDescriptor.t(); // now a row-vector
-			hogDescriptors.push_back(hogDescriptor);
+			hog_descriptors.push_back(hogDescriptor);
 		}
 		// concatenate all the descriptors for this sample vertically (into a row-vector):
-		hogDescriptors = hogDescriptors.reshape(0, hogDescriptors.cols * numLandmarks).t();
-		return hogDescriptors;
+		hog_descriptors = hog_descriptors.reshape(0, hog_descriptors.cols * num_landmarks).t();
+		return hog_descriptors;
 	};
 
 private:
 	vector<Mat> images;
-	VlHogVariant vlHogVariant;
-	int numCells;
-	int cellSize;
-	int numBins;
+	VlHogVariant vlhog_variant;
+	int num_cells;
+	int cell_size;
+	int num_bins;
 };
 
 /**
@@ -267,7 +267,7 @@ private:
  * @param[in] filename Path to a file with mean landmarks.
  * @return A cv::Mat of the loaded mean model.
  */
-cv::Mat loadMean(fs::path filename)
+cv::Mat load_mean(fs::path filename)
 {
 	std::ifstream file(filename.string());
 	if (!file.is_open()) {
@@ -280,9 +280,9 @@ cv::Mat loadMean(fs::path filename)
 	vector<string> values;
 	boost::split(values, line, boost::is_any_of(","));
 
-	int twiceNumLandmarks = static_cast<int>(values.size());
-	Mat mean(1, twiceNumLandmarks, CV_32FC1);
-	for (int i = 0; i < twiceNumLandmarks; ++i) {
+	int twice_num_landmarks = static_cast<int>(values.size());
+	Mat mean(1, twice_num_landmarks, CV_32FC1);
+	for (int i = 0; i < twice_num_landmarks; ++i) {
 		mean.at<float>(i) = std::stof(values[i]);
 	}
 
@@ -297,24 +297,24 @@ cv::Mat loadMean(fs::path filename)
  * perturbations of the initialisation.
  *
  * @param[in] mean Mean model points.
- * @param[in] faceBox A facebox to align the model to.
- * @param[in] scalingX Optional scaling in x of the model.
- * @param[in] scalingY Optional scaling in y of the model.
- * @param[in] translationX Optional translation in x of the model.
- * @param[in] translationY Optional translation in y of the model.
+ * @param[in] facebox A facebox to align the model to.
+ * @param[in] scaling_x Optional scaling in x of the model.
+ * @param[in] scaling_y Optional scaling in y of the model.
+ * @param[in] translation_x Optional translation in x of the model.
+ * @param[in] translation_y Optional translation in y of the model.
  * @return A cv::Mat of the aligned points.
  */
-Mat alignMean(Mat mean, cv::Rect faceBox, float scalingX=1.0f, float scalingY=1.0f, float translationX=0.0f, float translationY=0.0f)
+Mat align_mean(Mat mean, cv::Rect facebox, float scaling_x=1.0f, float scaling_y=1.0f, float translation_x=0.0f, float translation_y=0.0f)
 {
 	// Initial estimate x_0: Center the mean face at the [-0.5, 0.5] x [-0.5, 0.5] square (assuming the face-box is that square)
 	// More precise: Take the mean as it is (assume it is in a space [-0.5, 0.5] x [-0.5, 0.5]), and just place it in the face-box as
 	// if the box is [-0.5, 0.5] x [-0.5, 0.5]. (i.e. the mean coordinates get upscaled)
-	Mat alignedMean = mean.clone();
-	Mat alignedMeanX = alignedMean.colRange(0, alignedMean.cols / 2);
-	Mat alignedMeanY = alignedMean.colRange(alignedMean.cols / 2, alignedMean.cols);
-	alignedMeanX = (alignedMeanX*scalingX + 0.5f + translationX) * faceBox.width + faceBox.x;
-	alignedMeanY = (alignedMeanY*scalingY + 0.5f + translationY) * faceBox.height + faceBox.y;
-	return alignedMean;
+	Mat aligned_mean = mean.clone();
+	Mat aligned_mean_x = aligned_mean.colRange(0, aligned_mean.cols / 2);
+	Mat aligned_mean_y = aligned_mean.colRange(aligned_mean.cols / 2, aligned_mean.cols);
+	aligned_mean_x = (aligned_mean_x*scaling_x + 0.5f + translation_x) * facebox.width + facebox.x;
+	aligned_mean_y = (aligned_mean_y*scaling_y + 0.5f + translation_y) * facebox.height + facebox.y;
+	return aligned_mean;
 }
 
 /**
@@ -324,11 +324,11 @@ Mat alignMean(Mat mean, cv::Rect faceBox, float scalingX=1.0f, float scalingY=1.
  * @param[in] landmarks The landmarks to draw.
  * @param[in] color Color of the landmarks to be drawn.
  */
-void drawLandmarks(cv::Mat image, cv::Mat landmarks, cv::Scalar color = cv::Scalar(0.0, 255.0, 0.0))
+void draw_landmarks(cv::Mat image, cv::Mat landmarks, cv::Scalar color = cv::Scalar(0.0, 255.0, 0.0))
 {
-	auto numLandmarks = std::max(landmarks.cols, landmarks.rows) / 2;
-	for (int i = 0; i < numLandmarks; ++i) {
-		cv::circle(image, cv::Point2f(landmarks.at<float>(i), landmarks.at<float>(i + numLandmarks)), 2, color);
+	auto num_landmarks = std::max(landmarks.cols, landmarks.rows) / 2;
+	for (int i = 0; i < num_landmarks; ++i) {
+		cv::circle(image, cv::Point2f(landmarks.at<float>(i), landmarks.at<float>(i + num_landmarks)), 2, color);
 	}
 }
 
@@ -370,29 +370,29 @@ int main(int argc, char *argv[])
 		}
 		po::notify(vm);
 	}
-	catch (po::error& e) {
+	catch (const po::error& e) {
 		cout << "Error while parsing command-line arguments: " << e.what() << endl;
 		cout << "Use --help to display a list of options." << endl;
 		return EXIT_SUCCESS;
 	}
 
-	vector<Mat> trainingImages;
-	Mat trainingLandmarks;
+	vector<Mat> training_images;
+	Mat training_landmarks;
 	try	{
-		std::tie(trainingImages, trainingLandmarks) = loadIbugData(trainingset);
+		std::tie(training_images, training_landmarks) = load_ibug_data(trainingset);
 	}
-	catch (const fs::filesystem_error& ex)
+	catch (const fs::filesystem_error& e)
 	{
-		cout << ex.what() << endl;
+		cout << e.what() << endl;
 		return EXIT_FAILURE;
 	}
 
 	// Load the pre-calculated (and scaled) mean of all landmarks:
-	Mat modelMean = loadMean(meanfile);
+	Mat model_mean = load_mean(meanfile);
 	
 	// Load the face detector from OpenCV:
-	cv::CascadeClassifier faceCascade;
-	if (!faceCascade.load(facedetector.string()))
+	cv::CascadeClassifier face_cascade;
+	if (!face_cascade.load(facedetector.string()))
 	{
 		cout << "Error loading face detection model." << endl;
 		return EXIT_FAILURE;
@@ -400,11 +400,11 @@ int main(int argc, char *argv[])
 	
 	// Run the face detector and obtain the initial estimate x0 using the mean landmarks:
 	Mat x0;
-	for (size_t i = 0; i < trainingImages.size(); ++i) {
-		vector<cv::Rect> detectedFaces;
-		faceCascade.detectMultiScale(trainingImages[i], detectedFaces, 1.2, 2, 0, cv::Size(50, 50));
+	for (size_t i = 0; i < training_images.size(); ++i) {
+		vector<cv::Rect> detected_faces;
+		face_cascade.detectMultiScale(training_images[i], detected_faces, 1.2, 2, 0, cv::Size(50, 50));
 		// In a real application, you should verify that the detected face is not a false positive
-		x0.push_back(alignMean(modelMean, cv::Rect(detectedFaces[0])));
+		x0.push_back(align_mean(model_mean, cv::Rect(detected_faces[0])));
 	}
 
 	// We might want to augment the training set by perturbing the
@@ -416,17 +416,17 @@ int main(int argc, char *argv[])
 	regressors.emplace_back(LinearRegressor<>(Regulariser(Regulariser::RegularisationType::MatrixNorm, 2.0f, true)));
 	regressors.emplace_back(LinearRegressor<>(Regulariser(Regulariser::RegularisationType::MatrixNorm, 2.0f, true)));
 	
-	SupervisedDescentOptimiser<LinearRegressor<>> supervisedDescentModel(regressors);
+	SupervisedDescentOptimiser<LinearRegressor<>> supervised_descent_model(regressors);
 	
-	HogTransform hog(trainingImages, VlHogVariant::VlHogVariantUoctti, 3 /*numCells*/, 12 /*cellSize*/, 4 /*numBins*/);
+	HogTransform hog(training_images, VlHogVariant::VlHogVariantUoctti, 3 /*numCells*/, 12 /*cellSize*/, 4 /*numBins*/);
 
 	// Train the model. We'll also specify an optional callback function:
 	cout << "Training the model, printing the residual after each learned regressor: " << endl;
-	auto printResidual = [&trainingLandmarks](const cv::Mat& currentPredictions) {
-		cout << cv::norm(currentPredictions, trainingLandmarks, cv::NORM_L2) / cv::norm(trainingLandmarks, cv::NORM_L2) << endl;
+	auto print_residual = [&training_landmarks](const cv::Mat& current_predictions) {
+		cout << cv::norm(current_predictions, training_landmarks, cv::NORM_L2) / cv::norm(training_landmarks, cv::NORM_L2) << endl;
 	};
 	
-	supervisedDescentModel.train(trainingLandmarks, x0, Mat(), hog, printResidual);
+	supervised_descent_model.train(training_landmarks, x0, Mat(), hog, print_residual);
 
 	// To test on a whole bunch of images, we could do something roughly like this:
 	// supervisedDescentModel.test(x0_ts,
@@ -437,17 +437,17 @@ int main(int argc, char *argv[])
 	
 	// Detect the landmarks on a single image:
 	Mat image = cv::imread((trainingset / fs::path("image_0005.png")).string());
-	vector<cv::Rect> detectedFaces;
-	faceCascade.detectMultiScale(image, detectedFaces, 1.2, 2, 0, cv::Size(50, 50));
-	Mat initialAlignment = alignMean(modelMean, cv::Rect(detectedFaces[0]));
-	Mat prediction = supervisedDescentModel.predict(initialAlignment, Mat(), HogTransform({ image }, VlHogVariant::VlHogVariantUoctti, 3, 12, 4));
-	drawLandmarks(image, prediction, { 0, 255, 255 });
+	vector<cv::Rect> detected_faces;
+	face_cascade.detectMultiScale(image, detected_faces, 1.2, 2, 0, cv::Size(50, 50));
+	Mat initial_alignment = align_mean(model_mean, cv::Rect(detected_faces[0]));
+	Mat prediction = supervised_descent_model.predict(initial_alignment, Mat(), HogTransform({ image }, VlHogVariant::VlHogVariantUoctti, 3, 12, 4));
+	draw_landmarks(image, prediction, { 0, 255, 255 });
 	cv::imwrite("out.png", image);
 
 	// Save the learned model:
-	std::ofstream learnedModelFile("landmark_regressor_ibug_68lms.txt", std::ios::binary);
-	cereal::BinaryOutputArchive output_archive(learnedModelFile);
-	output_archive(supervisedDescentModel);
+	std::ofstream learned_model_file("landmark_regressor_ibug_68lms.txt", std::ios::binary);
+	cereal::BinaryOutputArchive output_archive(learned_model_file);
+	output_archive(supervised_descent_model);
 
 	return EXIT_SUCCESS;
 }
